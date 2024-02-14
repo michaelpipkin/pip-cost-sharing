f8f9c9073a1186c569f2bf478f1453d3271e146b
Michael Pipkin <michael.a.pipkin@gmail.com>
2024-02-13 23:00:45 -0500

    remove legacy material modules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,81 +1,33 @@
 import { NgModule } from '@angular/core';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
 import { MatCommonModule } from '@angular/material/core';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
-import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
-import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
-import { MatLegacyRadioModule as MatRadioModule } from '@angular/material/legacy-radio';
-import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatLegacySlideToggleModule as MatSlideToggleModule } from '@angular/material/legacy-slide-toggle';
-import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
 import { MatSortModule } from '@angular/material/sort';
-import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
-import { MatLegacyTabsModule as MatTabsModule } from '@angular/material/legacy-tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
 
 @NgModule({
   imports: [
-    MatButtonModule,
-    MatCardModule,
-    MatCheckboxModule,
     MatCommonModule,
     MatDatepickerModule,
-    MatDialogModule,
     MatDividerModule,
-    MatFormFieldModule,
     MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
     MatNativeDateModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatSelectModule,
     MatSidenavModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
     MatSortModule,
-    MatTableModule,
-    MatTabsModule,
     MatToolbarModule,
-    MatTooltipModule,
   ],
   exports: [
-    MatButtonModule,
-    MatCardModule,
-    MatCheckboxModule,
     MatCommonModule,
     MatDatepickerModule,
-    MatDialogModule,
     MatDividerModule,
-    MatFormFieldModule,
     MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
     MatNativeDateModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatSelectModule,
     MatSidenavModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
     MatSortModule,
-    MatTableModule,
-    MatTabsModule,
     MatToolbarModule,
-    MatTooltipModule,
   ],
 })
 export class MaterialModule {}
