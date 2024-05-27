22c0452c14fd86484d4085878ba653ed853ec8cc
michaelpipkin <michael.a.pipkin@gmail.com>
2024-05-27 18:56:44 -0400

    Deploy new version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   useEmulators: false,
-  buildDate: new Date('2024-05-25T07:48:34.226Z')
+  buildDate: new Date('2024-05-27T22:40:16.775Z')
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   useEmulators: false,
-  buildDate: new Date('2024-05-25T07:48:34.226Z'),
+  buildDate: new Date('2024-05-27T22:40:16.775Z')
 };
 
 /*
