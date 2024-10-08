8feeb3a86daf391b1afe865a7df0989f5aaf6108
michaelpipkin <michael.a.pipkin@gmail.com>
2024-10-08 04:40:58 -0400

    Remove unneeded dependencies

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   useEmulators: true,
-  buildDate: new Date('2024-10-07T17:03:19.221Z')
+  buildDate: new Date('2024-10-08T08:40:14.166Z')
 };
 
 /*
