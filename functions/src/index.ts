a2bd88ab049ed468e6090a8fca15f94d050cd4ed
michaelpipkin <michael.a.pipkin@gmail.com>
2025-01-07 15:45:57 -0500

    Add auto-delete receipt function; fix edit expense paid flag

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,11 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import { getHCaptchaSecret } from './common';
+
+admin.initializeApp();
+
+const db = admin.firestore();
+const storage = admin.storage();
 
 exports.validateHCaptcha = functions.https.onCall(async (request) => {
   const secret = await getHCaptchaSecret();
@@ -29,3 +35,48 @@ exports.validateHCaptcha = functions.https.onCall(async (request) => {
     );
   }
 });
+
+export const deleteOldPaidExpenses = functions.scheduler.onSchedule(
+  '0 0 * * *',
+  async (context) => {
+    try {
+      const [receipts] = await storage.bucket().getFiles();
+
+      for (const receipt of receipts) {
+        try {
+          const filePath = receipt.name; // This is the full path to the Firestore document
+          const expensePath = filePath.replace('receipts', 'expenses');
+          const expenseRef = db.doc(expensePath); // Adjust this to match your Firestore path
+          const expenseDoc = await expenseRef.get();
+
+          if (expenseDoc.exists) {
+            const expense = expenseDoc.data();
+
+            // Check if the expense is paid and older than 90 days
+            if (
+              !!expense &&
+              expense.paid &&
+              expense.date.toDate() <
+                new Date(Date.now() - 90 * 24 * 60 * 60 * 1000)
+            ) {
+              await receipt.delete();
+              console.log(`Successfully deleted file: ${filePath}`);
+
+              // Update the hasReceipt property to false
+              await expenseRef.update({ hasReceipt: false });
+              console.log(
+                `Updated hasReceipt to false for expense: ${expensePath}`
+              );
+            }
+          } else {
+            console.log(`Expense document not found for file: ${expensePath}`);
+          }
+        } catch (error) {
+          console.error(`Error processing file ${receipt.name}: `, error);
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching files from storage: ', error);
+    }
+  }
+);
