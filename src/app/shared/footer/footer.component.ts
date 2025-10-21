a76fd7fe2720ab7370db68b898fceb88a8739f03
michaelpipkin <michael.a.pipkin@gmail.com>
2025-10-21 12:03:42 -0400

    Make footer responsive

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,5 +1,6 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { DatePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
@@ -12,10 +13,21 @@ import packageJson from 'package.json';
   styleUrl: './footer.component.scss',
   imports: [DatePipe, MatButtonModule, RouterLink, MatTooltipModule],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  protected readonly breakpointObserver = inject(BreakpointObserver);
+
+  isSmallScreen = signal<boolean>(false);
   currentYear = signal<string>(new Date().getFullYear().toString());
   version = signal<string>(packageJson.version);
   buildDate = signal<Date>(environment.buildDate);
   production = signal<boolean>(environment.production);
   emulators = signal<boolean>(environment.useEmulators);
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe('(max-width: 1088px)')
+      .subscribe((result) => {
+        this.isSmallScreen.set(result.matches);
+      });
+  }
 }
