0b1e950396e090f3c91bb3b6daa5e2d74c5c0adf
michaelpipkin <michael.a.pipkin@gmail.com>
2025-10-21 11:57:54 -0400

    Consolidate footer version info

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
 import { environment } from '@env/environment';
 import packageJson from 'package.json';
@@ -9,7 +10,7 @@ import packageJson from 'package.json';
   selector: 'footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
-  imports: [DatePipe, MatButtonModule, RouterLink],
+  imports: [DatePipe, MatButtonModule, RouterLink, MatTooltipModule],
 })
 export class FooterComponent {
   currentYear = signal<string>(new Date().getFullYear().toString());
