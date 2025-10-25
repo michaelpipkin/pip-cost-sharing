a0443162b8d47ba762fa47ad40697bb38d82d0af
michaelpipkin <michael.a.pipkin@gmail.com>
2025-10-24 23:08:04 -0400

    Accomodate wider menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.breakpointObserver
-      .observe('(max-width: 1088px)')
+      .observe('(max-width: 1100px)')
       .subscribe((result) => {
         this.isSmallScreen.set(result.matches);
       });
