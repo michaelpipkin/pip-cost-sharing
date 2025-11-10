5f1f6deff0c1894407f91ea5b5db425d0d7a5d7e
michaelpipkin <michael.a.pipkin@gmail.com>
2025-11-10 16:06:40 -0500

    Add Google Play badging and website links

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, Signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink } from '@angular/router';
+import { PwaDetectionService } from '@services/pwa-detection.service';
 import { UserStore } from '@store/user.store';
 
 @Component({
@@ -13,6 +14,15 @@ import { UserStore } from '@store/user.store';
 export class HomeComponent {
   protected readonly userStore = inject(UserStore);
   protected readonly router = inject(Router);
+  protected readonly pwaDetection = inject(PwaDetectionService);
 
   isLoggedIn: Signal<boolean> = this.userStore.isLoggedIn;
+
+  isRunningInBrowser(): boolean {
+    return this.pwaDetection.isRunningInBrowser();
+  }
+
+  isRunningAsApp(): boolean {
+    return this.pwaDetection.isRunningAsApp();
+  }
 }
