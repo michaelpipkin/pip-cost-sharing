0d9a95625b173cb88c599b5cdf07f2965ebd82e9
michaelpipkin <michael.a.pipkin@gmail.com>
2025-12-04 14:24:57 -0500

    Add app camera functionality

---
+++ src/app/shared/file-selection-dialog/file-selection-dialog.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+
+export type FileSelectionOption = 'camera' | 'gallery' | 'file';
+
+export type FileSelectionDialogData = {
+  showCameraOption: boolean;
+  showGalleryOption: boolean;
+};
+
+@Component({
+  selector: 'app-file-selection-dialog',
+  templateUrl: './file-selection-dialog.component.html',
+  styleUrl: './file-selection-dialog.component.scss',
+  imports: [MatDialogModule, MatButtonModule, MatIconModule],
+})
+export class FileSelectionDialogComponent {
+  protected readonly data: FileSelectionDialogData = inject(MAT_DIALOG_DATA);
+}
